b6d0c6ab0f20953c3203ec5ed9ae73be544db28a
Jeyhun <ceyhunhajiyev@gmail.com>
2019-01-04 16:53:02 +0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -30,7 +30,7 @@ snake[0] = {
 
 let food = {
     x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box,
+    y : Math.floor(Math.random()*15+3) * box
 }
 
 // creat the score var
